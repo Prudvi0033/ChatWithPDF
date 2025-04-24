ff91baee236878d894e0c6a37e0d336125e770fa
Prudvi <prudvirajvarma2004@gmail.com>
2025-04-24 14:28:15 +0530

    Added chat route

---
+++ frontend/src/app/chat/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+
+    return (
+        <div className='grid grid-cols-2 text-white h-screen bg-black'>
+            <div>1</div>
+            <div className='border-l'>2</div>
+        </div>
+    )
+
+}
+
+export default page
--- frontend/src/components/ui/HeroSection.tsx
+++ frontend/src/components/ui/HeroSection.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button"
 import { Upload, Search } from "lucide-react"
 import { Montserrat } from "next/font/google"
+import Link from "next/link"
 
 const monte = Montserrat({ subsets: ["latin"] })
 
@@ -15,14 +16,13 @@ export default function HeroSection() {
                     Seamlessly upload your PDFs and let our AI do the heavy lifting — get instant, intelligent answers extracted in seconds.
                 </p>
                 <div className="flex flex-col sm:flex-row gap-4 justify-center">
-                    <Button
-                        size="lg"
-                        variant="outline"
-                        className="border-orange-200 text-orange-700 hover:bg-orange-100 cursor-pointer rounded-full px-8"
+                    <Link
+                        href='/chat'
+                        className={`border-orange-200 text-sm flex p-2 outline-1 font-semibold text-orange-700 hover:bg-orange-100 cursor-pointer rounded-full px-8 ${monte.className}`}
                     >
-                        <Upload className="mr-2 h-5 w-5" />
+                        <Upload className="mr-2 mt-0.5 h-4 w-4" />
                         Upload PDF
-                    </Button>
+                    </Link>
                 </div>
             </div>
         </section>
