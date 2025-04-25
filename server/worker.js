1972a8b477936105eab55a9c312564cbbaf9bedb
Prudvi <prudvirajvarma2004@gmail.com>
2025-04-25 15:37:34 +0530

    added worker

---
+++ server/worker.js
@@ -0,0 +1,13 @@
+import { Worker } from 'bullmq';
+
+const worker = new Worker(
+    'file-upload-queue',
+    async (job) => {
+        console.log("Job: ",job.data);
+    },
+    {concurrency : 100, connection : {
+        host : "localhost",
+        port : "6379"
+    }}
+)
+
