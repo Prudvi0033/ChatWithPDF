6d66a738db860aff4ee81afde4b417276e327340
Prudvi <prudvirajvarma2004@gmail.com>
2025-04-26 12:27:59 +0530

    Added qdarnt docker image

--- server/worker.js
+++ server/worker.js
@@ -1,9 +1,23 @@
 import { Worker } from 'bullmq';
+import { QdrantVectorStore } from "@langchain/qdrant";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { Document } from "@langchain/core/documents";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+
 
 const worker = new Worker(
     'file-upload-queue',
     async (job) => {
-        console.log("Job: ",job.data);
+        console.log("Job: ", job.data);
+        const data = JSON.parse(job.data)
+        
+        const loader = new PDFLoader(data.path)
+        const docs = await loader.load({})
+
+        const client = new QdrantClient({url : "http://localhost:6333"})
+        const embeddings = new OpenAIEmbeddings({apiKey : process.env.OPENAI_API_KEY})
+        
+        
     },
     {concurrency : 100, connection : {
         host : "localhost",
